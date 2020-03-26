53f2779a4c8658b158ca60b14f8358c3c977b6c8
Alexander Kraynov <kraynovsasha@gmail.com>
2020-03-26 21:38:09 +0300

    Initial commit
    added project files

---
+++ Event App/Event App/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Event App
+//
+//  Created by Admin on 26.03.2020.
+//  Copyright © 2020 eventapp. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
